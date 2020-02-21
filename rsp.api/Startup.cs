cb0ac4bf6db2787cc5cfc59f3da1abc82c689304
Mehmet Gani Tombalak <mgani.tombalak@yahoo.com>
2020-02-21 06:14:42 -0500

    Fix

--- rsp.api/Startup.cs
+++ rsp.api/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,7 +28,8 @@ namespace rsp.api
         {
             
             services.AddControllers();
-            services.AddEntityFrameworkNpgsql().AddDbContext<AerotiveDbContext>(builder =>
+            services.AddEntityFrameworkNpgsql()
+            .AddDbContext<ReservationDbContext>(builder =>
             {
                 builder.UseNpgsql(connectionString);
             });
