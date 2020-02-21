5aac4ac59ade16873e64e2b0722d2498ba39a596
Mehmet Gani Tombalak <mgani.tombalak@yahoo.com>
2020-02-21 02:56:25 -0500

    Interface addes

--- rsp.repository/base/BaseCrudRepository.cs
+++ rsp.repository/base/BaseCrudRepository.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 
-public abstract class BaseRepository<T, IdType> 
+public abstract class BaseRepository<T, IdType>  :IBaseRepository<T,IdType>
         where T : class, IEntity<IdType>
     {
         protected readonly ReservationDbContext currentDbcontext;
