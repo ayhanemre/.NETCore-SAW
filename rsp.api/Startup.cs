daa72a8ef30c2cde1f1ba2e4498e8e096829540b
Mehmet Gani Tombalak <mgani.tombalak@yahoo.com>
2020-02-21 06:31:15 -0500

    COnfiguration Params

--- rsp.api/Startup.cs
+++ rsp.api/Startup.cs
@@ -26,6 +26,11 @@ namespace rsp.api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            #region Configuration
+            
+            var connectionString = Configuration.GetConnectionString("reservationDb");
+            
+            #endregion
             
             services.AddControllers();
             services.AddEntityFrameworkNpgsql()
